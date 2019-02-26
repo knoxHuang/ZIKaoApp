fb46f4067f2d327c9831b2490f8a29948da8573b
knox <knox.h@qq.com>
2019-02-26 14:17:31 +0800

    update 2.0.6

--- main.9fca0.js
+++ main.9fca0.js
@@ -126,7 +126,7 @@ window.boot = function () {
         BK.Script.loadlib();
     }
     else {
-        var bundledScript = settings.debug ? 'src/project.dev.js' : 'src/project.8e1f5.js';
+        var bundledScript = settings.debug ? 'src/project.dev.js' : 'src/project.de58b.js';
         if (jsList) {
             jsList = jsList.map(function (x) {
                 return 'src/' + x;
@@ -162,7 +162,7 @@ window.boot = function () {
     cc.game.run(option, onStart);
 };
 
-// main.f0a67.js is qqplay and jsb platform entry file, so we must leave platform init code here
+// main.9fca0.js is qqplay and jsb platform entry file, so we must leave platform init code here
 if (false) {
     BK.Script.loadlib('GameRes://src/settings.js');
     BK.Script.loadlib();
@@ -188,12 +188,12 @@ if (false) {
 else if (window.jsb) {
     var isRuntime = (typeof loadRuntime === 'function');
     if (isRuntime) {
-        require('src/settings.5a997.js');
+        require('src/settings.c589b.js');
         require('src/cocos2d-runtime.js');
         require('jsb-adapter/engine/index.js');
     }
     else {
-        require('src/settings.5a997.js');
+        require('src/settings.c589b.js');
         require('src/cocos2d-jsb.js');
         require('jsb-adapter/jsb-engine.js');
     }
